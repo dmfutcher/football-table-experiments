4aae8fb3617048a4d35eaeb3fe7f57f1d7979641
David Futcher <bobbo@ubuntu.com>
2016-09-01 21:22:44 +0100

    Implement serving results data via /results endpoint

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import Promise from "bluebird";
+import fs from "fs";
+Promise.promisifyAll(fs);
 
 class FootballDataServer {
 
@@ -6,6 +9,19 @@ class FootballDataServer {
         this.app = express();
 
         this.app.use("/", express.static("./static"));
+        this.app.get("/results/:season", this.handleResultsRequest);
+    }
+
+    handleResultsRequest(req, res) {
+        const season = req.params.season;
+    
+        fs.readFileAsync(`./data/pl_results_${season}.json`)
+            .then((data) => {
+                res.send(data);
+            })
+            .catch((err) => {
+                res.json({ error: err });
+            })
     }
 
     serve() {
